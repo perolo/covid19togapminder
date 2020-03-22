6feeed10503ad638294ef14c83e02d4a64c2332a
perolo <per.olofsson@assaabloy.com>
2020-03-22 21:13:08 +0100

    Refactored code Added Relative series

--- covid19togapminder.go
+++ covid19togapminder.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +18,11 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+type csvFileType struct {
+	header []string
+	lines  map[string][]string
 }
 
 func main() {
@@ -34,19 +41,85 @@ func main() {
 	check(err)
 	defer ff.Close()
 
+	var csvFiles map[string]csvFileType
+	csvFiles = make(map[string]csvFileType)
 	files, err := ioutil.ReadDir(*dirPtr)
 	check(err)
-	first := true
 	for _, fil := range files {
 		fmt.Println(fil.Name())
 		if strings.HasSuffix(fil.Name(), "csv") {
-			convertCsvFile(ff, *dirPtr,fil.Name(),first)
-			first=false
+			var csvFile csvFileType
+			csvFile.lines = make(map[string][]string)
+			convertCsvFile(*dirPtr, fil.Name(), &csvFile)
+			csvFiles[fil.Name()] = csvFile
 		}
 	}
+	// TODO Check same header and Data
+	// Create Relative
+	relcsv := createRelCsv(csvFiles["time_series_19-covid-Deaths.csv"], csvFiles["time_series_19-covid-Confirmed.csv"], "Ratio: Death/Confirmed")
+	csvFiles["Ratio: Death/Confirmed"] = relcsv
+	relcsv2 := createRelCsv(csvFiles["time_series_19-covid-Recovered.csv"], csvFiles["time_series_19-covid-Confirmed.csv"], "Ratio: Recovered/Confirmed")
+	csvFiles["Ratio: Recovered/Confirmed"] = relcsv2
+
+	// PrintFile
+	first := true
+	for _, cfile := range csvFiles {
+		writeCsvFile(ff, first, &cfile)
+		first = false
+	}
+}
+func createRelCsv( tcsvf csvFileType, ncsvf csvFileType, name string) ( csvFileType){
+	var rsvfile csvFileType
+	rsvfile.lines = make(map[string][]string)
+	for _, lin := range tcsvf.lines {
+		dataName := lin[0]
+		for i, c := range lin {
+			if (i==0) {
+				rsvfile.lines[dataName] = []string{c}
+			} else if i==1 {
+				rsvfile.lines[dataName] = append(rsvfile.lines[dataName], name)
+			} else {
+				t, err := strconv.ParseFloat(c, 64);
+				check(err)
+				n, err := strconv.ParseFloat(ncsvf.lines[dataName][i], 64);
+				check(err)
+				if (n==0) {
+					rsvfile.lines[dataName] = append(rsvfile.lines[dataName], "0")
+				} else {
+					res := fmt.Sprintf("%.0f", (math.Round(1000.0 * t / n)))
+					rsvfile.lines[dataName] = append(rsvfile.lines[dataName], res)
+				}
+			}
+		}
+	}
+
+	return rsvfile
 }
 
-func convertCsvFile(f *os.File, adir string, acsvfile string, addheader bool) {
+func writeCsvFile(f *os.File, addheader bool, csvf *csvFileType) {
+	var err error
+	if addheader {
+		for _, h := range csvf.header {
+			_, err = f.WriteString(h+",")
+			check(err)
+		}
+		_, err = f.WriteString("\n")
+		check(err)
+	}
+	for _, lin := range csvf.lines {
+		for _, c := range lin {
+			_, err = f.WriteString(c+",")
+			check(err)
+		}
+		_, err = f.WriteString("\n")
+		check(err)
+	}
+	_, err = f.WriteString("\n")
+	check(err)
+
+}
+
+func convertCsvFile(adir string, acsvfile string, csvf *csvFileType) {
 	var err error
 	csvfile, err := os.Open(adir + string(os.PathSeparator) + acsvfile)
 	if err != nil {
@@ -64,50 +137,40 @@ func convertCsvFile(f *os.File, adir string, acsvfile string, addheader bool) {
 			break
 		}
 		check(err)
+		var dataName string
 		for k, rec := range record {
 			//assuming all files have same update date!?
-			if (addheader) {
-				if (k > 3) && (line == 0) { //"M/D/Y"
-					//ti, err := time.Parse("\"02/01/06\"", string(rec))
+			if line == 0 {
+				if k == 0 {
+					csvf.header = []string{record[0] + "-" + record[1]}
+				} else if k == 1 {
+					csvf.header = append(csvf.header, "Indicator")
+				} else if k == 2 {
+					//nothing
+				} else if k > 3 { //"M/D/Y"
 					ti, err := dateparse.ParseLocal(rec)
 					if err != nil {
 						panic(err.Error())
 					}
-					newdate := fmt.Sprintf("%d%02d%02d,", ti.Year(), ti.Month(), ti.Day())
-					_, err = f.WriteString(newdate)
-					check(err)
-				} else if (line == 0) && (k == 0) {
-					_, err = f.WriteString(record[0] + "-" + record[1] + ",")
-					check(err)
-				} else if (line == 0) && (k == 1) {
-					_, err = f.WriteString("Indicator,")
-					check(err)
-				} else if (line == 0) && (k == 2) {
-					//nothing
+					newdate := fmt.Sprintf("%d%02d%02d", ti.Year(), ti.Month(), ti.Day())
+					csvf.header = append(csvf.header, newdate)
 				}
-			}
-			if (line!=0) {
-				if k == 0 {
-					if rec == "" {
-						_, err = f.WriteString("Country" + "-" + strings.ReplaceAll(record[1],",","-") + ",")
-						check(err)
-					} else {
-						_, err = f.WriteString(strings.ReplaceAll(record[0],",","-")+ "-" + record[1] + ",")
-						check(err)
-					}
-				} else if k == 1 {
-					_, err = f.WriteString(strings.TrimSuffix(acsvfile,".csv" )+ ",")
-					check(err)
-				} else if (k == 2) || (k == 3) {
-					//nothing
+			} else if k == 0 {
+				if rec == "" {
+					dataName = "Country" + "-" + strings.ReplaceAll(record[1], ",", "-")
 				} else {
-					_, err = f.WriteString(rec + ",")
-					check(err)
+					dataName = strings.ReplaceAll(record[0], ",", "-") + "-" + record[1]
 				}
+				csvf.lines[dataName] = []string{dataName}
+			} else if k == 1 {
+				csvf.lines[dataName] = append(csvf.lines[dataName], strings.TrimSuffix(acsvfile, ".csv"))
+			} else if (k == 2) || (k == 3) {
+				//nothing
+			} else {
+				csvf.lines[dataName] = append(csvf.lines[dataName], record[4:]...)
+				break
 			}
 		}
-		_, err = f.WriteString("\n")
-		check(err)
 		line++
 	}
 }
